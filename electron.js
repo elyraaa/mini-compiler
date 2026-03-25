a665f8a3825ca94632d63b5adb5df4ee2d3ba24f
JAReyes86 <janandreireyes86@gmail.com>
2026-03-25 19:12:00 +0800

    Update electron.js
    psl work

--- electron.js
+++ electron.js
@@ -1,11 +1,12 @@
 const { app, BrowserWindow } = require('electron');
 const { exec } = require('child_process');
 const http = require('http');
+const path = require('path');
 
 let mainWindow;
 let nextProcess;
 
-function waitForServer(url, retries = 20, delay = 1000) {
+function waitForServer(url, retries = 30, delay = 1000) {
   return new Promise((resolve, reject) => {
     const attempt = () => {
       http.get(url, (res) => {
@@ -32,7 +33,17 @@ function createWindow() {
 }
 
 app.on('ready', () => {
-  nextProcess = exec('node node_modules/.bin/next start', { cwd: __dirname });
+  const appDir = path.dirname(app.getAppPath());
+  const nextBin = path.join(appDir, 'app', 'node_modules', '.bin', 'next');
+  const appPath = app.getAppPath();
+
+  nextProcess = exec(`node "${nextBin}" start`, {
+    cwd: appPath,
+    env: { ...process.env, NODE_ENV: 'production' }
+  });
+
+  nextProcess.stdout.on('data', (data) => console.log(data));
+  nextProcess.stderr.on('data', (data) => console.error(data));
 
   waitForServer('http://localhost:3000')
     .then(createWindow)
